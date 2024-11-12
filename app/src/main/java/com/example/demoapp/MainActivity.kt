6eb2e5264dfb16d46960c3bdd32ef488871d627e
sapmazum <sap11maz@gmail.com>
2024-11-13 00:20:19 +0530

    Changes done

---
+++ app/src/main/java/com/example/demoapp/MainActivity.kt
@@ -0,0 +1,67 @@
+package com.example.demoapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.demoapp.ui.theme.DemoAppTheme
+import androidx.compose.foundation.layout.*
+import androidx.compose.ui.unit.dp
+import androidx.compose.material3.Button
+import android.widget.Toast
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.platform.LocalContext
+
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            DemoAppTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+
+    val context = LocalContext.current
+
+    Column(
+        modifier = modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(text = "Hello, $name!")
+        Spacer(modifier = Modifier.height(16.dp))  // Adds space between text and button
+        Button(onClick = {
+            // Show a Toast message when the button is clicked
+            Toast.makeText(context, "Button clicked!", Toast.LENGTH_SHORT).show()
+        }) {
+            Text("Click Me")
+        }
+    }
+
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    DemoAppTheme {
+        Greeting("Android")
+    }
+}
